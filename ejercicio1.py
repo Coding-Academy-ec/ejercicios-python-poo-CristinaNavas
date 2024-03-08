594b56a26590d36d7d439fe4f0dfb509c5905776
CristinaNavas <cris.30a@hotmail.com>
2024-03-08 08:50:20 -0500

    Ejercicio 1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,6 +1,8 @@
 class Coche:
     def __init__(self, marca, modelo):
         # Asigna la marca proporcionada al atributo 'marca'
+        self.marca=marca
+        self.modelo=modelo
         # Asigna el modelo proporcionado al atributo 'modelo'
 
     def conducir(self):
